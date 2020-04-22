b84b5b9987e1142037229dbe4a8b030ee347df74
LinguList <mattis.list@uni-marburg.de>
2020-04-22 09:50:59 +0200

    update

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
         license="GPL",
         include_package_data=True,
         install_requires=['cldfbench', 'pyclts', 'lingpy', 'matplotlib'],
-        url='https://github.com/lingpy/sinopy',
+        url='https://github.com/lingpy/monolingual-borrowing-detection/',
         long_description=codecs.open('README.md', 'r', 'utf-8').read(),
         long_description_content_type='text/markdown',
         entry_points={

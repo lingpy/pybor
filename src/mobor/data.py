a8713a4703ab8da22efd8a2bdf751af0f61c271e
Tiago Tresoldi <tresoldi@shh.mpg.de>
2020-04-30 10:58:40 +0200

    Reformat; lint

--- src/mobor/data.py
+++ src/mobor/data.py
@@ -1,105 +1,111 @@
 """
 Wrapper for handling data in lexibank packages.
 """
+
+# Import Python standard libraries
 from importlib import import_module
 
+# Import MPI-SHH libraries
 import lingpy
 from pyclts import CLTS
 
+# TODO: Load with pycldf and not lingpy?
+# TODO: use a different logger (either standard or clldutils)
+
 class Wordlist(lingpy.Wordlist):
+    """
+    Class for loading data from a Lexibank dataset as a LingPy wordlist.
+    """
 
     @classmethod
     def from_lexibank(
-            self,
-            package,
-            fields=None,
-            fieldmapper=None,
-            fieldfunctions=None,
-            columns=(
-                "concept_name",
-                "concept_concepticon_id",
-                "language_id",
-                "language_glottocode",
-                "value",
-                "form",
-                "segments"
-                ),
-            namespace=(
-                ("concept_name", "concept"),
-                ("concept_concepticon_id", "concepticon"),
-                ("language_id", "doculect"),
-                ("language_glottocode", "glottocode"),
-                ("value", "value"),
-                ("form", "form"),
-                ("segments", "tokens")
-                )
-            ):
+        cls,
+        package,
+        fields=None,
+        fieldmapper=None,
+        fieldfunctions=None,
+        columns=(
+            "concept_name",
+            "concept_concepticon_id",
+            "language_id",
+            "language_glottocode",
+            "value",
+            "form",
+            "segments",
+        ),
+        namespace=(
+            ("concept_name", "concept"),
+            ("concept_concepticon_id", "concepticon"),
+            ("language_id", "doculect"),
+            ("language_glottocode", "glottocode"),
+            ("value", "value"),
+            ("form", "form"),
+            ("segments", "tokens"),
+        ),
+    ):
+        # Build/extend the list of columns to read
         if fieldmapper:
             for field, target in fieldmapper.items():
-                namespace += ((field, target))
+                namespace += (field, target)
         if fields:
             for field in fields:
                 columns += tuple([field])
-        module = import_module('lexibank_'+package)
-        lingpy.log.debug('imported module lexibank_'+package)
-        wordlist = self.from_cldf(
-                module.Dataset().cldf_dir.joinpath(
-                    'cldf-metadata.json'),
-                columns=columns,
-                namespace=namespace)
+
+        # Load data by importing the module
+        module = import_module("lexibank_" + package)
+        lingpy.log.debug("imported module lexibank_" + package)
+        wordlist = cls.from_cldf(
+            module.Dataset().cldf_dir.joinpath("cldf-metadata.json"),
+            columns=columns,
+            namespace=namespace,
+        )
         lingpy.log.info(
-            f'loaded wordlist {wordlist.height} concepts and {wordlist.width} languages'
-            )
+            f"loaded wordlist {wordlist.height} concepts and {wordlist.width} languages"
+        )
+
+        # Apply the field functions if provided
         if fieldfunctions:
             for field, function in fieldfunctions.items():
                 for idx in wordlist:
-                    wordlist[idx, field] = function(
-                            wordlist[idx, field])
+                    wordlist[idx, field] = function(wordlist[idx, field])
+
         return wordlist
 
-
-    def add_soundclass(self, model, clts=True):
-
+    def add_soundclass(cls, model, clts=True):
         # Use join to put as space delimited string not individual symbols.
         if clts:
             clts = CLTS()
-            soundclass = lambda x: ' '.join(clts.soundclass(model)(str(x)))
+            soundclass = lambda x: " ".join(clts.soundclass(model)(str(x)))
         else:
-            soundclass = lambda x: ' '.join(lingpy.tokens2class(x, model))
-        self.add_entries(
-                str(model),
-                'tokens',
-                soundclass)
+            soundclass = lambda x: " ".join(lingpy.tokens2class(x, model))
 
-    def add_formchars(self):
-    # Convert form to space delimited form without garbage symbols.
+        cls.add_entries(str(model), "tokens", soundclass)
+
+    def add_formchars(cls):
+        # Convert form to space delimited form without garbage symbols.
         formchars = lambda x: segmentchars(cleanform(x))
-        self.add_entries(
-                'formchars',
-                'form',
-                formchars)
 
+        cls.add_entries("formchars", "form", formchars)
 
-    def get_language(self,
-            language,
-            fields,
-            dtypes=None):
+    def get_language(cls, language, fields, dtypes=None):
+        """
+        Extract information for a given language.
+        """
+
         if not dtypes:
             dtypes = [str for field in fields]
         # Add sound class.
-        if 'tokens' in fields:
-        	self.add_soundclass(model='sca')
-        if 'form' in fields:
-        	self.add_formchars()
+        if "tokens" in fields:
+            cls.add_soundclass(model="sca")
+        if "form" in fields:
+            cls.add_formchars()
 
-        idxs = self.get_list(
-                col=language,
-                flat=True)
+        idxs = cls.get_list(col=language, flat=True)
         out = []
         for idx in idxs:
             row = [idx]
             for field, dtype in zip(fields, dtypes):
-                row += [dtype(self[idx, field])]
+                row += [dtype(cls[idx, field])]
 
             out += [row]
         return out
@@ -107,41 +113,43 @@ class Wordlist(lingpy.Wordlist):
 
 # Global functions instead of within class.
 def cleanform(word):
-# Clean word forms.
-# Does not solve problem of variants which would add words to table.
-	import re
-	pnre = re.compile('\(\d+\)')
-	space = re.compile('\s+')
-	question = re.compile('[?¿]')
-	affix = re.compile('(^-)|(-$)')
-	optional = re.compile('_?\(-?\w+-?\)_?')
+    # Clean word forms.
+    # Does not solve problem of variants which would add words to table.
+    import re
 
-	try:
-		word = word.strip().lower()
-		word = pnre.sub('', word).strip()
-		# previously used # for internal space.
-		word = space.sub('_', word)
-		word = question.sub('', word)
-		word = affix.sub('', word)
-		word = optional.sub('', word)
-		return word
-	except ValueError as error:
-		print('Value Error', error)
-		print(f'*{word}*')
-		return word
-	except Exception as exc:
-		print('Exception', exc)
-		print(f'*{word}*')
-		return word
+    pnre = re.compile(r'\(\d+\)')
+    space = re.compile(r'\s+')
+    question = re.compile(r'[?¿]')
+    affix = re.compile(r'(^-)|(-$)')
+    optional = re.compile(r'_?\(-?\w+-?\)_?')
+
+    try:
+        word = word.strip().lower()
+        word = pnre.sub("", word).strip()
+        # previously used # for internal space.
+        word = space.sub("_", word)
+        word = question.sub("", word)
+        word = affix.sub("", word)
+        word = optional.sub("", word)
+        return word
+    except ValueError as error:
+        print("Value Error", error)
+        print(f"*{word}*")
+        return word
+    except Exception as exc:
+        print("Exception", exc)
+        print(f"*{word}*")
+        return word
+
 
 def segmentchars(form):
-	try:
-		return ' '.join(list(form))
-	except ValueError as error:
-		print('Value Error', error)
-		print(f'*{form}*')
-		return form
-	except Exception as exc:
-		print('Exception', exc)
-		print(f'*{form}*')
-		return form
+    try:
+        return " ".join(list(form))
+    except ValueError as error:
+        print("Value Error", error)
+        print(f"*{form}*")
+        return form
+    except Exception as exc:
+        print("Exception", exc)
+        print(f"*{form}*")
+        return form

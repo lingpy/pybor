80a062bc8213286c1fe6fb0d0293682bb748dbb0
Johann-Mattis List <LinguList@users.noreply.github.com>
2020-05-05 16:52:03 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,7 @@ import pathlib
 from setuptools import setup, find_packages, Extension
 
 # setup package name etc as a default
-<<<<<<< HEAD
 pkgname = 'pybor'
-=======
-pkgname = "mobor"
->>>>>>> 2f99cf15fcdae21b844fab7868d160e90324a250
 
 # The directory containing this file
 LOCAL_PATH = pathlib.Path(__file__).parent
@@ -20,7 +16,6 @@ with open(REQUIREMENTS_PATH.as_posix()) as fp:
     install_requires = [dep.strip() for dep in fp.readlines()]
 
 setup(
-<<<<<<< HEAD
         name=pkgname,
         description="A Python library for monolingual borrowing detection.",
         version='0.1.1',
@@ -40,30 +35,3 @@ setup(
         author_email='list@shh.mpg.de',
         keywords='borrowing, language contact, sequence comparison'
         )
-=======
-    author="John Miller",
-    classifiers=[
-        "License :: OSI Approved :: GPL License",
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-        "Topic :: Software Development :: Libraries",
-    ],
-    description="A Python library for monolingual borrowing detection.",
-    entry_points={"console_scripts": ["mobor=mobor.cli:main"]},
-    include_package_data=True,
-    install_requires=install_requires,
-    keywords="borrowing, language contact, sequence comparison",
-    license="GPL",
-    long_description_content_type="text/markdown",
-    long_description=README_CONTENTS,
-    name=pkgname,
-    package_dir={"": "src"},
-    packages=find_packages(where="src"),
-    python_requires=">=3.7",
-    test_suite="tests",
-    tests_require=[],
-    url="https://github.com/lingpy/monolingual-borrowing-detection/",
-    version="0.1.1",
-    zip_safe=False,
-)
->>>>>>> 2f99cf15fcdae21b844fab7868d160e90324a250

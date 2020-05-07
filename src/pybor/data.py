bd71d438b07e63f57b8ee4df7048d3d3d0a7fbeb
LinguList <mattis.list@posteo.de>
2020-05-07 19:25:14 +0200

    update

--- src/pybor/data.py
+++ src/pybor/data.py
@@ -2,89 +2,61 @@
 Wrapper for handling data in lexibank packages.
 """
 from importlib import import_module
-
-import lingpy
 from pyclts import CLTS
+from csvw.dsv import UnicodeDictReader
+from tqdm import tqdm
 
-class Wordlist(lingpy.Wordlist):
+
+class LexibankDataset(object):
     
-    @classmethod
-    def from_lexibank(
-            self,
-            package,
-            fields=None,
-            fieldmapper=None,
-            fieldfunctions=None,
-            columns=(
-                "concept_name",
-                "concept_concepticon_id",
-                "language_id",
-                "language_glottocode",
-                "value",
-                "form",
-                "segments"
-                ),
-            namespace=(
-                ("concept_name", "concept"),
-                ("concept_concepticon_id", "concepticon"),
-                ("language_id", "doculect"),
-                ("language_glottocode", "glottocode"),
-                ("value", "value"),
-                ("form", "form"),
-                ("segments", "tokens")
-                )
-            ):
-        if fieldmapper:
-            for field, target in fieldmapper.items():
-                namespace += ((field, target))
-        if fields:
-            for field in fields:
-                columns += tuple([field])
+    def __init__(self, package, transform=None):
+        """
+        Load the data of a lexibank dataset.
+        """
+        clts = CLTS()
+        modify = {
+                'Tokens': lambda x, y, z: x['Segments'].split(),
+                'Language': lambda x, y, z: y[x['Language_ID']]['Name'],
+                'Glottocode': lambda x, y, z: y[x['Language_ID']]['Glottocode'],
+                'Concept': lambda x, y, z: z[x['Parameter_ID']]['Name'], 
+                'Concepticon_ID': lambda x, y, z: z[x['Parameter_ID']]['Concepticon_ID'], 
+                'Concepticon_GLOSS': lambda x, y, z: z[x['Parameter_ID']]['Concepticon_Gloss'], 
+                'FormChars': lambda x, y, z: list(x['Form']),
+                'ASJP': lambda x, y, z: clts.soundclass('asjp')(x['Segments']),
+                'DOLGO': lambda x, y, z: clts.soundclass('dolgo')(x['Segments']),
+                'SCA': lambda x, y, z: clts.soundclass('sca')(x['Segments']),
+                }
+        transform = transform or {}
+        modify.update(transform)
         module = import_module('lexibank_'+package)
-        lingpy.log.debug('imported module lexibank_'+package)
-        wordlist = self.from_cldf(
-                module.Dataset().cldf_dir.joinpath(
-                    'cldf-metadata.json'),
-                columns=columns,
-                namespace=namespace)
-        lingpy.log.info(
-            f'loaded wordlist {wordlist.height} concepts and {wordlist.width} languages'
-            )
-        if fieldfunctions:
-            for field, function in fieldfunctions.items():
-                for idx in wordlist:
-                    wordlist[idx, field] = function(
-                            wordlist[idx, field])
-        return wordlist
+        self.ds = module.Dataset()
+        self.forms = []
+        self.concepts = {}
+        with UnicodeDictReader(
+                self.ds.cldf_dir.joinpath('parameters.csv')) as reader:
+            for row in reader:
+                self.concepts[row['ID']] = row
+        self.languages = {}
+        with UnicodeDictReader(
+                self.ds.cldf_dir.joinpath('languages.csv')) as reader:
+            for row in reader:
+                self.languages[row['ID']] = row
 
-    def add_soundclass(self, model, clts=True):
+        with UnicodeDictReader(
+                self.ds.cldf_dir.joinpath('forms.csv')) as reader:
+            for row in tqdm(reader, desc='loading data'):
+                for key, fun in modify.items():
+                    row[key] = fun(row, self.languages, self.concepts)
+                self.forms.append(row)
 
-        if clts:
-            clts = CLTS()
-            soundclass = lambda x: clts.soundclass(model)(str(x))
-        else:
-            soundclass = lambda x: lingpy.tokens2class(x, model)
-            
-        self.add_entries(
-                str(model),
-                'tokens',
-                soundclass)
-
-    def get_language(self,
-            language,
-            fields,
-            dtypes=None):
-        if not dtypes:
-            dtypes = [str for field in fields]
-        idxs = self.get_list(
-                col=language,
-                flat=True)
+    def get_table(self, language=None, form='Form', classification='Borrowed'):
         out = []
-        for idx in idxs:
-            row = [idx]
-            for field, dtype in zip(fields, dtypes):
-                row += [dtype(self[idx, field])]
-            
-            out += [row]
+        for row in self.forms:
+            if not language or row['Language'] == language:
+                out.append([row['ID'], row[form], row[classification]])
         return out
+
+
+
+
 
